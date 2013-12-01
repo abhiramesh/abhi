82eac6ba69a1c10f7699a05330088d3e72537afd
Abhi Ramesh <abhiramesh@gmail.com>
2013-12-01 01:32:04 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abhi::Application.config.secret_token = '7685ea8520d5d6e991093e4d9f3b8a5fa4e090045d5e30e3aeb468d872aa3d2db37551ea22428af9714afb7ea42a01915a6b82a62a2fe09470244d7e3e842b19'
